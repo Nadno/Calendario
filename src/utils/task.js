f4346605ab5d9c409694a80d43aa626df15b05f8
Nadno <eunadno@gmail.com>
2020-12-19 12:36:35 -0300

    task refactored

--- src/utils/task.js
+++ src/utils/task.js
@@ -1,50 +1,53 @@
-import CalendarData from "./calendar";
+import { getDate } from "../date";
 
-class Task extends CalendarData {
-  get(callback) {
-    const { day, month, year } = this.position;
-    const TYPE = "tasks";
+const ITEM_TYPE = "tasks";
+
+const task = (calendar) => {
+  
+
+  calendar.getTask = function getTask(callback) {
+    const { day, month, year } = getDate("selected");
 
     if (day) {
-      this.checkIfDayExists();
-      this.selected = this.calendar[year][month][day][TYPE];
+      calendar.checkIfDayExists();
+      calendar.selected = calendar.data[year][month][day][ITEM_TYPE];
     } else {
-      this.selected = this.calendar.daily;
+      calendar.selected = calendar.data.daily;
     }
+
+
   
-    if (callback) return callback(Object.assign([], this.selected), TYPE);
-    return this;
+    if (callback) return callback(Object.assign([], calendar.selected), ITEM_TYPE);
   }
 
-  update(position, item, value) {
-    if (this.selected[position]) this.selected[position][item] = value;
+  // update(position, item, value) {
+  //   if (this.selected[position]) this.selected[position][item] = value;
 
-    this.save();
-    return this;
-  }
+  //   this.save();
+  //   return this;
+  // },
 
-  create(text) {
-    if (!this.selected.length) {
-      this.selected.push({ text, checked: false });
+  // create(text) {
+  //   if (!this.selected.length) {
+  //     this.selected.push({ text, checked: false });
 
-      this.removeMarkFromDays().setDayWithItems();
-      this.save();
-      return this;
-    }
-    this.selected.push({ text, checked: false });
+  //     removeMarkFromDays().setDayWithItems();
+  //     return this;
+  //   }
+  //   this.selected.push({ text, checked: false });
 
-    this.save();
-    return this;
-  }
+  //   this.save();
+  //   return this;
+  // },
 
-  delete(from, to) {
-    this.selected.splice(from, to);
-    if (!this.selected.length)
-      this.removeMarkFromDays().setDayWithItems();
+  // delete(from, to) {
+  //   this.selected.splice(from, to);
+  //   if (!this.selected.length)
+  //     this.removeMarkFromDays().setDayWithItems();
 
-    this.save();
-    return this;
-  }
-}
+  //   this.save();
+  //   return this;
+  // },
+};
 
-export default new Task();
+export default task;
